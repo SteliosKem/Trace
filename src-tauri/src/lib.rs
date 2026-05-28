edcc389e98c5c92f4aa84d0e4bba6abb9c288bf5
SteliosKem <stylianoskem@gmail.com>
2026-05-28 19:49:19 +0300

    Fix Windows Corners

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -32,9 +32,34 @@ pub fn run() {
             {
                 use tauri::Manager;
                 use window_vibrancy::apply_acrylic;
+                use windows_sys::Win32::Foundation::HWND;
+                use windows_sys::Win32::Graphics::Dwm::{
+                    DwmSetWindowAttribute, DWMWA_BORDER_COLOR,
+                    DWMWA_WINDOW_CORNER_PREFERENCE, DWMWCP_DONOTROUND,
+                };
 
                 let window = app.get_webview_window("main").unwrap();
                 apply_acrylic(&window, Some((18, 18, 20, 90))).ok();
+
+                if let Ok(hwnd) = window.hwnd() {
+                    let hwnd = hwnd.0 as HWND;
+                    let prefer_no_round: u32 = DWMWCP_DONOTROUND;
+                    let border_none: u32 = 0xFFFFFFFE; // DWMWA_COLOR_NONE
+                    unsafe {
+                        DwmSetWindowAttribute(
+                            hwnd,
+                            DWMWA_WINDOW_CORNER_PREFERENCE as u32,
+                            &prefer_no_round as *const _ as *const _,
+                            std::mem::size_of::<u32>() as u32,
+                        );
+                        DwmSetWindowAttribute(
+                            hwnd,
+                            DWMWA_BORDER_COLOR as u32,
+                            &border_none as *const _ as *const _,
+                            std::mem::size_of::<u32>() as u32,
+                        );
+                    }
+                }
             }
             Ok(())
         })
